77e33ee752a0160d4114b55950748b69dc68d2f7
misa5555 <misa.gk@icloud.com>
2014-08-10 13:49:35 -0700

    test

--- word_chains.rb
+++ word_chains.rb
@@ -1,3 +1,15 @@
 class WordChainer
+  def initialize(dictionary_file_name)
+    @dictionary = IO.readlines(dictionary_file_name).map{|e|e.chomp!}
+  end
 
+
+
+  def adjacent_words(word)
+    @dictionary.each do |d_word|
+      ad_words = []
+      next unless d_word.length == word.length
+      ad_words << d_word
+    end
+  end
 end
