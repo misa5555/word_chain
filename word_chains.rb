edf74d5586e88f725f5fe605b9d5f6b577f2508f
misa5555 <misa.gk@icloud.com>
2014-08-10 14:22:21 -0700

    finished phase 1

--- word_chains.rb
+++ word_chains.rb
@@ -1,15 +1,24 @@
+require 'set'
 class WordChainer
   def initialize(dictionary_file_name)
-    @dictionary = IO.readlines(dictionary_file_name).map{|e|e.chomp!}
+    @dictionary = IO.readlines(dictionary_file_name).map{|e|e.chomp!}.to_set
   end
-
-
 
   def adjacent_words(word)
-    @dictionary.each do |d_word|
-      ad_words = []
-      next unless d_word.length == word.length
-      ad_words << d_word
+    adjacent_words = []
+    word.each_char.each_with_index do |old_char, idx|
+      ("a".."z").each do |new_char|
+        next if old_char == new_char
+        new_word = word.dup
+        new_word[idx] = new_char
+        adjacent_words << new_word if @dictionary.include?(new_word)
+      end
     end
+    adjacent_words
   end
+
 end
+
+w = WordChainer.new("dictionary.txt")
+p w.adjacent_words("bat")
+
